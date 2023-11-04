623b0f4232c2929e1e2dfade0afd4ad7b0bb72d5
aashraf09 <asad15112007@gmail.com>
2023-11-05 03:08:11 +0500

    grid, flex-box, margins and paddings added

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/index.css";
+import "@/styles/globals/grids.css";
+import "@/styles/globals/typography.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <div
+        className="flex flex-col items-start justify-center"
+        style={{ border: "1px solid red" }}
+      >
+        <h1 className="">sfiughsiufgy</h1>
+        <p>hgiauyfriuyg</p>
+      </div>
+    </>
+  );
 };
 
 export default index;
